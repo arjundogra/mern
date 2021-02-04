a46639502bfd849aaeb322bc9f1e21df1df18512
Arjun Dogra <arjundogra146@gmail.com>
2021-02-04 16:11:49 +0530

    form

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const handleSubmit = () => {
+    alert("Hello");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form">
+        <h2 className="head">Sign Up</h2>
+        <div className="formData">
+          <label className="label">Name</label>
+          <input type="text" placeholder="Name" />
+        </div>
+        <div className="formData">
+          <label className="label">Email Id</label>
+          <input type="email" placeholder="Email Id" />
+        </div>
+        <div className="formData">
+          <label className="label">Password</label>
+          <input type="password" placeholder="Password" />
+        </div>
+        <div className="formData">
+          <label className="label">Confirm Password</label>
+          <input type="password" placeholder="Confirm Password" />
+        </div>
+        <button type="button" onClick={handleSubmit}>
+          Sign Up
+        </button>
+      </div>
     </div>
   );
 }
